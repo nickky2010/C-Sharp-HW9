2435175259b4174b0bb6fdaa74723056a26c9a8b
Mikalai <nickky2010@mail.ru>
2019-07-03 18:07:49 +0300

    Delete IArea.cs

--- HW9_1/AnacondaFramework/Interfaces/IArea.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace AnacondaFramework.Interfaces
-{
-    public interface IArea : ISized, IDisplayable
-    {
-        IBox[] GetBoxes();
-        void CreatBox(int count);
-        void ShowBox();
-    }
-}
